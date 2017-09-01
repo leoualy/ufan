9a14ffef190cefe45f8973260a03347bd7698263
test <test@qq.com>
2017-09-01 15:50:45 +0800

    添加配置操作

--- app/init.go
+++ app/init.go
@@ -1,7 +1,45 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"gopkg.in/yaml.v2"
+	"io/ioutil"
+	"os/user"
+	"path"
+)
+
+type ConfDefault struct {
+	Host string `host`
+	Port int    `port`
+	Url  string `url`
+}
+
+func getExecPath() string {
+	curUser, err := user.Current()
+	dir := curUser.HomeDir
+	if nil == err {
+		return dir
+	}
+	return "err"
+}
+
+func (c *ConfDefault) getConf() *ConfDefault {
+	fmt.Println("当前用户:", getExecPath())
+	// 组合配置文件路径
+	dirCfg := path.Join(getExecPath(), "default.yaml")
+	fileCfg, err := ioutil.ReadFile(dirCfg)
+	if err != nil {
+		fmt.Println(err)
+	}
+	err = yaml.Unmarshal(fileCfg, c)
+	if err != nil {
+		fmt.Println(err)
+	}
+	return c
+}
 
 func main() {
-	fmt.Println("filename:init.go")
+	var c ConfDefault
+	c.getConf()
+	fmt.Println(c)
 }
