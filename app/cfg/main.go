8b1e75a36aa51474d1813c0e2913a11e34d62ca2
test <test@qq.com>
2017-09-02 20:59:07 +0800

    修改配置

---
+++ app/cfg/main.go
@@ -0,0 +1,41 @@
+package cfg
+
+import (
+	"fmt"
+	"io/ioutil"
+	"os/user"
+	"path"
+
+	"gopkg.in/yaml.v2"
+)
+
+type CfgDefault struct {
+	Host string `host`
+	Post int    `port`
+	Url  string `url`
+}
+
+func GetHomeDir() string {
+	curUser, err := user.Current()
+	dir := curUser.HomeDir
+	if nil == err {
+		return dir
+	}
+	return err.Error()
+}
+
+func GetCfgDefault() CfgDefault {
+	var c = CfgDefault{}
+	dirCfg := path.Join(GetHomeDir(), "default.yaml")
+	fileCfg, err := ioutil.ReadFile(dirCfg)
+	if err != nil {
+		fmt.Println(err)
+		return c
+	}
+	err = yaml.Unmarshal(fileCfg, &c)
+	if err != nil {
+		fmt.Println(err)
+		return c
+	}
+	return c
+}
